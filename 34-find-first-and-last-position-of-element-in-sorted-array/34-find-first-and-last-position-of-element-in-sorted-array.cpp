376142a03745da182675eabeb36e76862f8da351
sakshammahale2000 <102991372+sakshammahale2000@users.noreply.github.com>
2022-04-07 12:37:30 +0530

    Time: 14 ms (33.54%), Space: 13.5 MB (98.31%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,7 +1,97 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
+    vector<int> searchRange(vector<int>& nums, int target)
+    {   
         int n=nums.size();
+        int str=0,mid=-1;
+        int end=n-1;
+        int flag=0;
+        int f=-1,l=-1;
+        int midf,midl;
+        while(str<=end)
+          {
+                
+                mid=(str+end)/2;
+                if(target==nums[mid])
+                {  flag=1;
+                    midf=mid;
+                    end=mid-1;
+                    continue;
+                    
+                } 
+                else if(target>nums[mid])
+                     str=mid+1;
+                else
+                     end=mid-1;
+            
+           }
+        if(flag==0)
+            return {-1,-1};
+        cout<<midf<<" "<<mid;
+       
+        str=midf;end=n-1;
+         while(str<=end)
+          {
+                
+                mid=(str+end)/2;
+                if(target==nums[mid])
+                {  
+                    midl=mid;
+                    str=mid+1;
+                    continue;
+                    
+                } 
+                else if(target>nums[mid])
+                     str=mid+1;
+                else
+                     end=mid-1;
+            
+           }
+
+        f=midf;
+        l=midl;
+        return {f,l};
+        
+    
+  
+    }
+};
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+      
+     /*   midf=mid;
+        midl=mid;
+        while(nums[midf]==target)
+        {
+            if(nums[midf-1]==target)
+            {
+                f=midf-1;
+                midf--;
+            }
+        }
+         while(nums[midl]==target)
+        {
+            if(nums[midl-1]==target)
+            {
+                f=midl+1;
+                midl++;
+            }
+        }*/
+/* int n=nums.size();
         int f=-1,l=-1;
         if(nums.empty()||nums[n-1]<target||nums[0]>target)
         {
@@ -17,6 +107,4 @@ public:
               l=i;
           }
         }
-        return {f,l};
-    }
-};
+        return {f,l};*/
